295c645d6e2bcced40c4ec6bb46bc9d21ec07d91
venzym <venzym@naver.com>
2022-08-27 22:38:13 +0900

    [42587] 프린터

---
+++ Programmers/bumjin/프린터.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        Queue<Printer> q = new LinkedList();
+
+        for (int i=0; i<priorities.length; i++) {
+            q.add(new Printer(i, priorities[i]));
+        }
+
+        while (!q.isEmpty()) {
+            int priority = q.peek().priority;
+            boolean flag = false;
+
+            for (Printer p : q) {
+                if (p.priority > priority) {
+                    //중요도 높은 문서 존재
+                    flag = true;
+                }
+            }
+
+            if (flag) {
+                q.add(q.poll());
+            } else {
+                if (q.poll().location == location) {
+                    //대기목록 길이 - q 길이
+                    //q poll()로 계속 작아짐
+                    answer = priorities.length - q.size();
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    static class Printer {
+        int location, priority;
+
+        Printer(int location, int priority) {
+            this.location = location;
+            this.priority = priority;
+        }
+    }
+}
