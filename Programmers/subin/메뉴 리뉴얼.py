186841ff949c8107845ee59c636079c45de9cdd0
subinmun1997 <qzxy812@gmail.com>
2022-09-22 21:46:50 +0900

    [72411] 메뉴 리뉴얼

---
+++ Programmers/subin/메뉴 리뉴얼.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+from collections import Counter
+
+
+def solution(orders, course):
+    answer = []
+    for c in course:
+        temp = []
+        for order in orders:
+            combi = combinations(sorted(order), c)
+            temp += combi
+        counter = Counter(temp)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
+
+    return sorted(answer)
