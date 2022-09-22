985b04b2934de718f14c0dfff6032bc5ba75624f
yong-dev <tjddyd1565@naver.com>
2022-09-22 22:36:41 +0900

    [72411] 메뉴 리뉴얼

--- Programmers/seongyong/메뉴 리뉴얼.py
+++ Programmers/seongyong/메뉴 리뉴얼.py
@@ -4,22 +4,26 @@ from collections import defaultdict
 
 def solution(orders, course):
     answer = []
-    orders = list(map(set, orders))
+    set_orders = list(map(set, orders))
     for n in course:
-        dict_n_combo = defaultdict(list[tuple])  # check if error
+        dict_n_combo = defaultdict(list)  #dictionary : key = count of combo, value = combo
         visited = set()
-        for i in orders:
-            for combo in combinations(i, n): #combination이 자꾸 겹쳐서 나옴
+        for order in orders: # count possible combo while iterating orders
+            for combo in combinations(order, n):
+                combo = tuple(sorted(combo))
                 if combo not in visited:
                     visited.add(combo)
                     count = 0
-                    for order in orders:
-                        if set(combo).issubset(order): count += 1
+                    for set_order in set_orders:
+                        if set(combo).issubset(set_order): count += 1
 
-                    if count >= 2: dict_n_combo[count].append(combo)
+                    if count >= 2: dict_n_combo[count].append(''.join(combo)) 
 
-        if len(dict_n_combo) != 0:
-            answer.extend(sorted(dict_n_combo.items())[-1][1])
+        if len(dict_n_combo) != 0: # append max order to answer
+            dict_n_combo = sorted(dict_n_combo.items())
+            max_ordered = dict_n_combo[-1][0]
+            while dict_n_combo and dict_n_combo[-1][0] == max_ordered: # consider multiple max order
+                answer.extend(dict_n_combo.pop()[1])
 
     answer.sort()
     return answer
