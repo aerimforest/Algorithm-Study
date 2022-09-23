e1781cccca2e20af4ce8c6016d4e6cc975ed3d70
yong-dev <tjddyd1565@naver.com>
2022-09-23 22:35:42 +0900

    [81302] 거리두기 확인하기

---
+++ Programmers/seongyong/거리두기 확인하기.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+def bfs(p):
+    start = []
+    
+    for i in range(5): # 시작점이 되는 P 좌표 구하기
+        for j in range(5):
+            if p[i][j] == 'P':
+                start.append([i, j])
+    
+    for s in start:
+        queue = deque([s])  # 큐에 초기값
+        visited = [[0]*5 for i in range(5)]   # 방문 처리 리스트
+        distance = [[0]*5 for i in range(5)]  # 경로 길이 리스트
+        visited[s[0]][s[1]] = 1
+        
+        while queue:
+            y, x = queue.popleft()
+        
+            dx = [-1, 1, 0, 0]  # 좌우
+            dy = [0, 0, -1, 1]  # 상하
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0<=nx<5 and 0<=ny<5 and visited[ny][nx] == 0:
+                    
+                    if p[ny][nx] == 'O':
+                        queue.append([ny, nx])
+                        visited[ny][nx] = 1
+                        distance[ny][nx] = distance[y][x] + 1
+                    
+                    if p[ny][nx] == 'P' and distance[y][x] <= 1:
+                        return 0
+    return 1
+
+
+def solution(places):
+    answer = []
+    
+    for i in places:
+        answer.append(bfs(i))
+    
+    return answer
