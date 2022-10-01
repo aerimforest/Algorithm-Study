a789a11562a5d1d18c921a7a05bf8b8fb59f5efd
aerimforest <aerimforest98@gmail.com>
2022-10-01 22:43:40 +0900

    [42627] 디스크 컨트롤러

---
+++ Programmers/yerim/디스크 컨트롤러.cpp
@@ -0,0 +1,29 @@
+// [42627] 디스크 컨트롤러
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(const vector<int> &a, const vector<int> &b) 
+{
+    return a[1] <= b[1];
+}
+
+int solution(vector<vector<int>> jobs) {
+    int n = jobs.size(), answer = 0, currentTime = 0;
+    sort(jobs.begin(), jobs.end(), cmp);
+
+    while(!jobs.empty()) {
+        for(int i = 0; i < jobs.size(); i++) {
+            if(jobs[i][0] <= currentTime) {
+                currentTime += jobs[i][1];
+                answer += (currentTime - jobs[i][0]);
+                jobs.erase(jobs.begin() + i);
+                break;
+            }
+            if(i == jobs.size() - 1) currentTime++;
+        }
+    }
+    
+    return answer / n;
+}
