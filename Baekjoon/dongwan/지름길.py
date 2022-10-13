1af7cdeaedf2bf396c15b52d54382c8022b54418
asdf134652 <asdf134652@gmail.com>
2022-10-13 19:24:32 +0900

    [1446] 지름길

--- Baekjoon/dongwan/지름길.py
+++ Baekjoon/dongwan/지름길.py
@@ -10,16 +10,16 @@ def solution(x) :
     distance[x] = 0 
 
     while heap :
-        dist,now = heapq.heappop(heap)
+        cost,now = heapq.heappop(heap)
 
-        if dist > distance[now] :
+        if cost > distance[now] :
             continue
         for i in graph[now] :
-            cost = dist + i[1]
-            if cost < distance[i[0]] :
-                distance[i[0]] = cost 
+            new_cost = cost + i[0]
+            if new_cost < distance[i[1]] :
+                distance[i[1]] = new_cost 
 
-                heapq.heappush(heap,(cost,i[0]))
+                heapq.heappush(heap,(new_cost,i[1]))
 
 
 #지름길, 고속도로 길이
@@ -30,14 +30,14 @@ graph = [[] for _ in range(end+1)]
 distance = [INF] * (end+1)
 
 for i in range(end) :
-    graph[i].append((i+1,1))
+    graph[i].append((1,i+1))
 
 for _ in range(N) :
     s,e,l = map(int,input().split())
 
     if e > end :
         continue
-    graph[s].append((e,l))
+    graph[s].append((l,e))
 
 solution(0)
 
