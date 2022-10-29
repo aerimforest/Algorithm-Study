31f0d1ed0ec091fe3cd432a5bd91553e2ab3f5d7
magnolia55 <46524118+magnolia55@users.noreply.github.com>
2022-10-30 00:36:59 +0900

    [1092] 배 (수정)

--- Baekjoon/hyeonyoung/배.cpp
+++ Baekjoon/hyeonyoung/배.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 int N, Crane[51], M, Box[10001];
+bool moved[10001];
 
 int main()
 {
@@ -25,7 +26,39 @@ int main()
     }
     sort(Box, Box + M);
 
-    
+    int ans = 0;
+    if (Box[M - 1] > Crane[N - 1])
+    {
+        ans = -1;
+    }
+    else
+    {
+        int cnt = 0;
+        while (cnt < M)
+        {
+            ans++;
+            int idx = M;
+            for (int i = N - 1; i >= 0; --i)
+            {
+                for (idx--; idx >= 0; --idx)
+                {
+                    if (moved[idx])
+                    {
+                        continue;
+                    }
+
+                    if (Crane[i] >= Box[idx])
+                    {
+                        moved[idx] = true;
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << ans;
 
     return 0;
 }
