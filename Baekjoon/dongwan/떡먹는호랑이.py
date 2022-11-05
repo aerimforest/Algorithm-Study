958f539e71f6034825a7efc1499143e5226d5d56
holawan <asdf134652@gmail.com>
2022-11-05 18:06:30 +0900

    [2502] 떡먹는 호랑이

---
+++ Baekjoon/dongwan/떡먹는호랑이.py
@@ -0,0 +1,22 @@
+d,k = map(int,input().split())
+
+# x3 = x1+x2
+# x4 = x2+x3
+dp = [0 for i in range(6)]
+dp[0],dp[1] = 1,1
+
+while True:
+    for i in range(2,d):
+        dp[i] = dp[i-1]+dp[i-2]
+    
+    if dp[d-1] == k:
+        break
+    elif dp[-1] > k:
+        dp[0] += 1
+        dp[1] = dp[0]
+    else:
+        dp[1] += 1
+
+print(dp[0])
+print(dp[1])
+
