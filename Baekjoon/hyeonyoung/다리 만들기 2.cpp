6a7dce6ccae9fefbe098e63c4ac7089ae70b9cc8
magnolia55 <46524118+magnolia55@users.noreply.github.com>
2022-11-07 17:04:43 +0900

    [17472] 다리 만들기 2 (수정)

--- Baekjoon/hyeonyoung/다리 만들기 2.cpp
+++ Baekjoon/hyeonyoung/다리 만들기 2.cpp
@@ -5,8 +5,19 @@
 
 using namespace std;
 
-int N, M, board[11][11];
-int island[11][11], bridge[7][7];
+int N, M, board[11][11], answer = 0;
+int island[11][11], bridge[11][11];
+bool connect[11];
+
+struct state
+{
+    int A, B, br;
+
+    bool operator<(const state &X) const
+    {
+        return this->br > X.br;
+    }
+};
 
 const int dx[] = {-1, 1, 0, 0};
 const int dy[] = {0, 0, -1, 1};
@@ -53,7 +64,7 @@ int main()
 
                         if (board[xx][yy] == 1 && island[xx][yy] == 0)
                         {
-                            island[xx][yy] = cnt;
+                            island[xx][yy] = island[x][y];
                             q.push({xx, yy});
                         }
                     }
@@ -62,19 +73,21 @@ int main()
         }
     }
 
+    // cout << "\n";
     // for (int i = 0; i < N; ++i)
     // {
     //     for (int j = 0; j < M; ++j)
     //     {
-    //         cout << island[i][j];
+    //         cout << island[i][j] << " ";
     //     }
     //     cout << "\n";
     // }
+    // cout << "\n";
 
     // 2. 가능한 다리 찾기
-    for (int i = 1; i <= cnt; ++i)
+    for (int i = 0; i <= cnt; ++i)
     {
-        for (int j = 1; j <= cnt; ++j)
+        for (int j = 0; j <= cnt; ++j)
         {
             bridge[i][j] = INF;
         }
@@ -90,15 +103,108 @@ int main()
             {
                 len++;
             }
+            else if (len <= 1)
+            {
+                len = 0;
+                A = island[i][j];
+            }
             else
             {
                 B = island[i][j];
-                bridge[A][B] = min(bridge[A][B], len);
+                bridge[A][B] = bridge[B][A] = min(bridge[A][B], len);
                 A = B;
+                len = 0;
             }
         }
     }
+
     // 2-2. 세로 다리 찾기
+    for (int j = 0; j < M; ++j)
+    {
+        int len = 0, A = 0, B = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            if (island[i][j] == 0)
+            {
+                len++;
+            }
+            else if (len <= 1)
+            {
+                len = 0;
+                A = island[i][j];
+            }
+            else
+            {
+                B = island[i][j];
+                bridge[A][B] = bridge[B][A] = min(bridge[A][B], len);
+                A = B;
+                len = 0;
+            }
+        }
+    }
+
+    // for (int i = 1; i <= cnt; ++i)
+    // {
+    //     for (int j = 1; j <= cnt; ++j)
+    //     {
+    //         if (bridge[i][j] == INF)
+    //         {
+    //             cout << "- ";
+    //         }
+    //         else
+    //         {
+    //             cout << bridge[i][j] << " ";
+    //         }
+    //     }
+    //     cout << "\n";
+    // }
+    // cout << "\n";
+
+    // 3. 다리 연결하기
+    int bridge_cnt = 0;
+    priority_queue<state> pq;
+    connect[1] = 1;
+    for (int i = 1; i <= cnt; ++i)
+    {
+        if (bridge[1][i] < INF)
+        {
+            pq.push({1, i, bridge[1][i]});
+        }
+    }
+    while (!pq.empty() && bridge_cnt < cnt - 1)
+    {
+        int A = pq.top().A, B = pq.top().B, len = pq.top().br;
+        pq.pop();
+
+        if (connect[A] && connect[B])
+        {
+            continue;
+        }
+
+        if (!connect[B])
+        {
+            swap(A, B);
+        }
+
+        // B에 A를 추가
+        connect[A] = 1;
+        for (int i = 1; i <= cnt; ++i)
+        {
+            if (connect[i] == false && bridge[A][i] < INF)
+            {
+                pq.push({A, i, bridge[A][i]});
+            }
+        }
+
+        // cout << A << " " << B << " " << bridge[A][B] << "\n";
+        answer += len;
+        bridge_cnt++;
+    }
+    if (bridge_cnt < cnt - 1)
+    {
+        answer = -1;
+    }
+    cout << answer;
 
     return 0;
 }
