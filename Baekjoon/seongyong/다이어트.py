d2e1c070f4056a3cda07e07633a4bce74d4c4929
Soeng-dev <tjddyd1565@naver.com>
2022-11-08 21:16:17 +0900

    [17140] 이차원 배열과 연산

---
+++ Baekjoon/seongyong/다이어트.py
@@ -0,0 +1,78 @@
+import sys
+from collections import *
+
+
+def read_line():
+    return sys.stdin.readline().rstrip()
+
+
+def split_n_map(split_target, seperator, to_map):
+    return map(to_map, split_target.split(seperator))
+
+
+def read_n_map(to_map, seperator):
+    return split_n_map(read_line(), seperator, to_map)
+
+
+def read_line_as(return_type, element_type, seperator):
+    return return_type(read_n_map(element_type, seperator))
+
+
+def read_lines(line_number, trim_each_line=None):
+    if trim_each_line:
+        return [trim_each_line(read_line()) for _ in range(line_number)]
+    else:
+        return [read_line() for _ in range(line_number)]
+
+
+def read_lines_as(row_type, col_type, seperator, line_number):
+    return [read_line_as(row_type, col_type, seperator)
+            for _ in range(line_number)]
+
+
+def add_iter(iter0, iter2, return_type):
+    return return_type(sum(e) for e in zip(iter0, iter2))
+
+
+# visited = set()
+
+# def dfs_return(dfs_result, current_position):
+#     visited.remove(current_position)
+#     return dfs_result
+
+def find_root(target, root_of: list):
+    if root_of[target] == target:
+        return target
+
+    root_of[target] = find_root(root_of[target], root_of)
+    return root_of[target]
+
+
+def union(x, y, root_of: list):
+    x, y = find_root(x, root_of), find_root(y, root_of)
+    if x != y:
+        bigger, smaller = max(x, y), min(x, y)
+        root_of[bigger] = smaller
+
+
+dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+# main logic
+g = int(read_line())
+now, past = 2, 1
+ans = list()
+while 2 * now - 1 <= g:
+    diff = now ** 2 - past ** 2
+    if diff == g:
+        ans.append(now)
+        now+=1
+        past+=1
+    elif diff < g:
+        now += 1
+    else:  # diff > g
+        past += 1
+
+if not ans:
+    print(-1)
+else:
+    for w in ans: print(w)
