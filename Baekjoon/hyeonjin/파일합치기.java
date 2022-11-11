2ef05002b8d97bbba1fb4080b7729d8d942044b4
이현진 <ehj1107@naver.com>
2022-11-11 22:47:08 +0900

    [11066]파일 합치기

---
+++ Baekjoon/hyeonjin/파일합치기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    /*
+    모든 경우의 수를 다확인 해야한다고 생각.
+    DFS나 BFS를 생각했는데 숫자가 너무 많아서 오버플로우 나올 것 같음.
+    그리고 계산하는 과정에서 중복이 발생됨.
+    예제에서 40 30 30 50이면
+    40 + 30 / 30 / 50
+    후에 앞의 70값과 30을 더할때와 50을 더할때 각각 70이 중복됨.
+    그래서 다이나믹 프로그래밍으로 풀어야 할 것 같음.
+
+    아직 dp 문제를 많이 못풀어봐서 그런지 점화식을 세우는게 잘안되서 못풀었습니다.
+     */
+}
