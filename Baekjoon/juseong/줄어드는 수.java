8eaaa97fc7c3170001bcd341af632455e8c01c9e
Juseong Kim <kjschocolate@gmail.com>
2022-11-27 22:53:33 +0900

    [1174] 줄어드는 수

---
+++ Baekjoon/juseong/줄어드는 수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        // 0 1 2 3 4 5 6 7 8 9
+        // 10 20 21 30 31 32 40 41 42 43 50 51 52 53 54... 98
+        // 210 310 321 320 321 410 420 421 430 431 432 ... 987
+        //
+    }
+}
