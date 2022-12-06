700b723b232da0e8fb91c7636817189da45f1bbf
Soeng-dev <tjddyd1565@naver.com>
2022-12-06 22:43:41 +0900

    [15565] 귀여운 라이언

---
+++ Baekjoon/seongyong/귀여운 라이언.py
@@ -0,0 +1,76 @@
+import sys
+from collections import *
+
+
+def read_line():
+    return sys.stdin.readline().rstrip()
+
+
+def split_n_map(split_target, seperator, to_map):
+    return map(to_map, split_target.split(seperator))
+
+
+def read_n_map(to_map, seperator):
+    return split_n_map(read_line(), seperator, to_map)
+
+
+def read_line_as(return_type, element_type, seperator):
+    return return_type(read_n_map(element_type, seperator))
+
+
+def read_lines(line_number, trim_each_line=None):
+    if trim_each_line:
+        return [trim_each_line(read_line()) for _ in range(line_number)]
+    else:
+        return [read_line() for _ in range(line_number)]
+
+
+def read_lines_as(row_type, col_type, seperator, line_number):
+    return [read_line_as(row_type, col_type, seperator)
+            for _ in range(line_number)]
+
+
+def add_iter(iter0, iter2, return_type):
+    return return_type(sum(e) for e in zip(iter0, iter2))
+
+
+# visited = set()
+
+# def dfs_return(dfs_result, current_position):
+#     visited.remove(current_position)
+#     return dfs_result
+
+def find_root(target, root_of: list):
+    if root_of[target] == target:
+        return target
+
+    root_of[target] = find_root(root_of[target], root_of)
+    return root_of[target]
+
+
+def union(x, y, root_of: list):
+    x, y = find_root(x, root_of), find_root(y, root_of)
+    if x != y:
+        bigger, smaller = max(x, y), min(x, y)
+        root_of[bigger] = smaller
+
+
+dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+# main logic
+n, k = read_n_map(int, ' ')
+dolls = read_line_as(list, int, ' ')
+
+ryan_indexes = list()
+for i, doll in enumerate(dolls):
+    if doll == 1:
+        ryan_indexes.append(i)
+
+if len(ryan_indexes) < k:
+    size = -1
+else:
+    size = len(dolls)
+    for start, end in zip(ryan_indexes, ryan_indexes[k - 1:]):
+        size = min(end + 1 - start, size)
+
+print(size)
