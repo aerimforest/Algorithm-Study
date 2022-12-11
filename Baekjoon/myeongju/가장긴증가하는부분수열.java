bcffde87bf10f416a1854bedefce4f963a94f5dc
mmmmjjj <myungju25@naver.com>
2022-12-11 14:44:31 +0900

    [11053] 가장 긴  증가하는 부분 수열

---
+++ Baekjoon/myeongju/가장긴증가하는부분수열.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        Arrays.fill(dp, 1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 1;
+        for (int i = 1; i < N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    if (dp[i] > answer) {
+                        answer = dp[i];
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
