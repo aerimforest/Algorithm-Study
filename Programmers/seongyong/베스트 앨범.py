7164e17de575e2ceff6ce6e45d790126eb94be2b
Seong yong , Kim <69845367+Soeng-dev@users.noreply.github.com>
2022-12-13 22:28:27 +0900

    [42579] 베스트 앨범

--- Programmers/seongyong/베스트 앨범.py
+++ Programmers/seongyong/베스트 앨범.py
@@ -1,17 +1,31 @@
-from collections import defaultdict
-
 def solution(genres, plays):
-    answer = []
-    
-    genres_order = defaultdict(int)
-    genres_plays = defaultdict(list)
-    
-    for i, (genre, v) in enumerate(zip(genres, plays)):
-        genres_order[genre] += v
-        genres_plays[genre].append((i, v))
-        
-    for genre, _ in sorted(genres_order.items(), key = lambda x: x[1], reverse = True):
-        for i, v in sorted(genres_plays[genre], key = lambda x: x[1], reverse = True)[:2]:
-            answer.append(i)
-    
-    return answer
+
+    import operator
+
+    play_info = dict()
+    album = list()
+
+    for i in range(len(genres)):
+        if genres[i] in play_info:
+            play_info[genres[i]][0] += plays[i]
+            play_info[genres[i]][1][i] = plays[i]
+        else:
+            play_info[genres[i]] = list()
+            play_info[genres[i]].insert(0, plays[i])
+            play_info[genres[i]].insert(1, {i: plays[i]})
+
+    play_info = sorted(play_info.items(),
+                       key=operator.itemgetter(1),
+                       reverse=True)
+
+    for genre_info in play_info:
+        play_time = genre_info[1][1]
+        play_time = sorted(play_time.items(),
+                           key=operator.itemgetter(1),
+                           reverse=True)
+
+        album.append(play_time[0][0])
+        if len(play_time) > 1:
+            album.append(play_time[1][0])
+
+    return album
