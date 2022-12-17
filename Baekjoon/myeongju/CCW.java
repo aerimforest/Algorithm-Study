ec3e22668b4eb620a672d9c0fe0c63f80844087e
mmmmjjj <myungju25@naver.com>
2022-12-17 11:41:27 +0900

    [11758] CCW

---
+++ Baekjoon/myeongju/CCW.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int x3 = Integer.parseInt(st.nextToken());
+        int y3 = Integer.parseInt(st.nextToken());
+
+        int a = x1 * y2 + x2 * y3 + x3 * y1;
+        int b = y1 * x2 + y2 * x3 + y3 * x1;
+
+        if(a-b>0) System.out.println("1");
+        else if (a==b) System.out.println("0");
+        else System.out.println("-1");
+
+    }
+
+}
