8928a7518adc9680efc6c8d4030cd27ba3e0d731
Juseong Kim <kjschocolate@gmail.com>
2023-01-04 22:46:52 +0900

    [3079] 입국심사

--- Baekjoon/juseong/입국심사.java
+++ Baekjoon/juseong/입국심사.java
@@ -4,15 +4,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static long[] gate;
+    static long ans;
+    static long max;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 사람의 수, 1 ≤ N ≤ 100,000
-        int M = Integer.parseInt(st.nextToken()); // 심사대의 수, 1 ≤ M ≤ 1,000,000,000
-        int[] gate = new int[M]; // 심사대별 소요 시간
-        for (int i = 0; i < M; i++) {
-            gate[i] = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 입국심사대의 개수 (1 ≤ N ≤ 100,000)
+        M = Integer.parseInt(st.nextToken()); // 사람의 수 (1 ≤ M ≤ 1,000,000,000)
+        gate = new long[N]; // k번째 심사대가 심사하는데 걸린 시간(1 ≤ Tk ≤ 10^9)
+        for (int i = 0; i < N; i++) {
+            gate[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, gate[i]);
         }
-        // 모든 사람이 심사를 마치는데 걸리는 시간의 최솟값을 출력한다.
+        // R = 사람수 * 입국 심사대에서 걸리는 최대 값
+        long L = 0, R = M * max, mid = 0;
+        while(L <= R) {
+            // [L ... R] 범위 안에 답이 존재
+            mid = (L + R) / 2; // 심사는 하는데 걸린 시간
+            if (determination(mid)) { // 해당 시간에 심사가 가능하면
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static boolean determination(long mid) {
+        long cnt = 0; // 심사 가능한 사람의 수
+        for (int i = 0; i < gate.length; i++) { // 심사대
+            cnt += mid / gate[i]; // 심사대 별 가능한 사람 수
+        }
+        return M <= cnt; // 사람의 수 <= 심사 가능한 사람의 수
     }
 }
