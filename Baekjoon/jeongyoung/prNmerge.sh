9a5793518431940e3c1a125d2bd4e5feba3ff643
Sunergy <qpalzmm22@gmail.com>
2023-02-04 12:04:52 +0900

    [20056] 마법사 상어와 파이어볼

--- Baekjoon/jeongyoung/prNmerge.sh
+++ Baekjoon/jeongyoung/prNmerge.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
-# Requirements : Read README file first and follow the instructions
+# Requirements : Do following instructions before executing this.
+# 1. install `gh cli`
+# 2. login with `gh auth login`
+# 3. stage and commit the changes
 
 # This shell script automatically creates pull request
 # with today's date and labels, 
 # and merges with https://github.com/Sigmaflo/Algorithm-Study
+
+# FIX THIS AS YOU LIKE
+tags="c++,jeongyoung"
 
 gh repo set-default Sigmaflo/Algorithm-Study
 
 # Assumes that the commits are already pushed but just in case
 git push
 
-tags="c++,jeongyoung"
 today=`date +"%y-%m-%d"`
 
 echo "Today : $today"
 
 # pull request
-prNum=`gh pr create --title "[Baekjoon] $today" -l $tags -f | cut -b -50`
+prNum=`gh pr create --title "[Baekjoon] $today" -l $tags -f -b ""`
 
-echo "pr Num : $prNum"
+echo "$prNum"
 # merge
 gh pr merge -m $prNum
 
