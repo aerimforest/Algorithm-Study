28e8293c81b49b239a06a1e8bb5d8c1d0b2da513
Juseong Kim <kjschocolate@gmail.com>
2023-02-04 20:31:44 +0900

    [1194] 달이 차오른다 가자(update)

--- Baekjoon/juseong/달이 차오른다 가자.java
+++ Baekjoon/juseong/달이 차오른다 가자.java
@@ -6,76 +6,84 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, ans;
-    static boolean[][][] visited;
+    static int n, m, sx, sy, ans;
     static char[][] A;
-    static Queue<Dot> Q;
-    static int[][] dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        A = new char[N][];
-        visited = new boolean[N][M][1<<6];
-        for (int i = 0; i < N; i++) A[i] = br.readLine().toCharArray();
-    }
-
-    static class Dot {
-        int x, y, key;
-
-        public Dot(int x, int y, int key) {
-            this.x = x;
-            this.y = y;
-            this.key = key;
-        }
-    }
-
-    static void bfs() {
-        int dist = 0;
-        while(!Q.isEmpty()) {
-            int size = Q.size();
-            dist++;
-            for (int s = 0; s < size; s++) {
-                Dot dot = Q.poll();
-                int x = dot.x, y = dot.y, nowKey = dot.key;
-                if (A[x][y] == '1') {
-                    ans = dist - 1;
-                    return;
-                }
-                for (int k = 0; k < 4; k++) {
-                    int nx = x + dir[k][0], ny = y + dir[k][1];
-                    if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                    if (A[nx][ny] == '#') continue;
-                    if (visited[nx][ny][nowKey]) continue;
-                    if (A[nx][ny] >= 'a' && A[nx][ny] <= 'f') { // 열쇠를 만났을 때
-                        int newKey = 1<<(A[nx][ny] - 'a');
-                        int addedKey = nowKey | newKey;
-                        visited[nx][ny][addedKey] = true;
-                        Q.add(new Dot(nx, ny, addedKey));
-                    } else if (A[nx][ny] >= 'A' && A[nx][ny] <= 'F') { // 문을 만났을 때
-                        int door = 1 << (A[nx][ny] - 'A');
-                        if ((door & nowKey) > 0) { // 열쇠가 있으면
-                            visited[nx][ny][nowKey] = true;
-                            Q.add(new Dot(nx, ny, nowKey));
-                        }
-                    } else {
-                        visited[nx][ny][nowKey] = true;
-                        Q.add(new Dot(nx, ny, nowKey));
-                    }
-                }
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        A = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                A[i][j]= s.charAt(j);
             }
         }
-    }
+   }
+
+   static class Dot {
+        int x, y, key;
+
+       public Dot(int x, int y, int key) {
+           this.x = x;
+           this.y = y;
+           this.key = key;
+       }
+   }
+
+   static void bfs() {
+        Queue<Dot> Q = new LinkedList<>();
+        boolean[][][] visited = new boolean[n][m][1 << 6];
+        Q.add(new Dot(sx, sy, 0)); // 시작점을 큐에 넣어준다
+        visited[sx][sy][0] = true; // 시작점 방문 확인
+        int move = 0; // 이동 횟수
+        while (!Q.isEmpty()) {
+           int size = Q.size();
+           for (int s = 0; s < size; s++) {
+               Dot dot = Q.poll();
+               int x = dot.x, y = dot.y, key = dot.key;
+               if (A[x][y] == '1') { // 탈출구를 만났을 때
+                   ans = move; // 도착점 이동횟수 갱신
+                   return; // bfs 종료
+               }
+               for (int k = 0; k < 4; k++) {
+                   int nx = x + dir[k][0], ny = y + dir[k][1];
+                   if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                   if (A[nx][ny] == '#') continue;
+                   if (A[nx][ny] >= 'a' && A[nx][ny] <= 'f') { // 열쇠를 만났을 때
+                       int newKey = key | (1 << (A[nx][ny] - 'a')); // 새로운 열쇠 계산
+                       if (visited[nx][ny][newKey]) continue; // 다음칸을 방문 했으면
+                       visited[nx][ny][newKey] = true; // 다음칸 방문 확인
+                       Q.add(new Dot(nx, ny, newKey)); // 다음칸을 큐에 넣어준다
+                   } else if (A[nx][ny] >= 'A' && A[nx][ny] <= 'F') { // 문을 만났을 때
+                       int door = 1 << (A[nx][ny] - 'A'); // 문값 계산
+                       if ((key & door) > 0) { // 문을 열수 있으면
+                           if (visited[nx][ny][key]) continue; // 다음칸을 방문 했으면
+                           visited[nx][ny][key] = true; // 다음칸 방문 확인
+                           Q.add(new Dot(nx, ny, key)); // 다음칸을 큐에 넣어준다
+                       }
+                   } else { // A[nx][ny] == '.', 빈칸을 만났을 때
+                        if (visited[nx][ny][key]) continue; // 다음칸을 방문 했으면
+                       visited[nx][ny][key] = true;  // 다음칸 방문 확인
+                        Q.add(new Dot(nx, ny, key)); // 다음칸을 큐에 넣어준다
+                   }
+               }
+           }
+           move++; // 이동 횟수 증가
+       }
+   }
 
     static void pro() {
-        Q = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        // 민식이를 찾는다
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (A[i][j] == '0') {
-                    Q.add(new Dot(i, j, 0));
-                    visited[i][j][0] = true;
+                    sx = i;
+                    sy = j;
+                    A[i][j] = '.'; // 민식이의 위치를 빈칸으로 바꿔준다
                 }
             }
         }
