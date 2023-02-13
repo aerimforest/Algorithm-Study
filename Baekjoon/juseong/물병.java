4b531edaec62f2ffa96695c266d068be1cdc77f7
Juseong Kim <kjschocolate@gmail.com>
2023-02-13 22:55:15 +0900

    Create 물병.java

---
+++ Baekjoon/juseong/물병.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n, k;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        k = scanner.nextInt();
+    }
+}
