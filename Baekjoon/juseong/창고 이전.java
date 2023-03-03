cf908d2000ae40bd791c3b997b1245ac85a6557c
Juseong Kim <kjschocolate@gmail.com>
2023-03-03 22:55:44 +0900

    [2134] 창고 이전

---
+++ Baekjoon/juseong/창고 이전.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] A = new int[n+1];
+        int[] B = new int[m+1];
+    }
+}
